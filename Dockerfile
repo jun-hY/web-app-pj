508de18e407c6bc8737283c36caf474f9a3c3bbe
jun-hy <dlvm789@gmail.com>
2025-05-08 15:25:55 +0900

    update: add sqlite3 package

--- Dockerfile
+++ Dockerfile
@@ -10,9 +10,11 @@ RUN apt-get update
 RUN apt-get install git -y
 RUN npm install express
 RUN npm i -D typescript @types/express @types/node
-RUN git clone https://github.com/jun-hY/web-app-pj.git
+# RUN git clone https://github.com/jun-hY/web-app-pj.git
+COPY ./ ./
 
-WORKDIR /usr/src/app/web-app-pj
+
+# WORKDIR /usr/src/app/web-app-pj
 
 RUN npx tsc
 
--- server.ts
+++ server.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from 'express'
+import path from 'path';
 
 const PORT: number = 8080;
-
 const app: Express = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('typescript test')
-})
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/', (req: Request, res: Response) => {
+
+});
 
 app.listen(PORT, () => {
     console.log('Server is running');
-})
+});
