f442a4d259d788adbf51c659a15470b3d6b0bc88
jun-hy <dlvm789@gmail.com>
2025-05-12 02:58:47 +0900

    feature: initDb function

--- db/query.ts
+++ db/query.ts
@@ -3,10 +3,13 @@ import db from "./database"
 export const initDb = (): void => {
     db.serialize(() => {
         db.run(`CREATE TABLE IF NOT EXISTS boards (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            TEXT NOT NULL,
-            email TEXT UNIQUE NOT NULL
+            id INTEGER PRIMARY KEY,
+            advantage TEXT NOT NULL,
+            improve TEXT NOT NULL,
+            point TEXT NOT NULL,
+            summary TEXT NOT NULL,
+            improved_code TEXT NOT NULL
         )`);
-    });
+    }); 
 };
 
