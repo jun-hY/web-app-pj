3af9d9fb8880eb713b2bc7768c1a13348215e7f1
jun-hy <dlvm789@gmail.com>
2025-05-12 02:58:23 +0900

    refactor: database.ts

--- db/database.ts
+++ db/database.ts
@@ -1,10 +1,10 @@
-import sqlite3 from "sqlite3";
+import { Database, verbose } from "sqlite3";
 import path from "path";
 
-sqlite3.verbose();
+verbose();
 
 const dbPath = path.join(__dirname, '../data/database.sqlite3'); // DB 파일 위치
-const db = new sqlite3.Database(dbPath, (err) => {
+const db = new Database(dbPath, (err) => {
     if (err) {
         console.error('Could not connect to database', err);
     } else {
