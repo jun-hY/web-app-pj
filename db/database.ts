b4d58277d047e1fcb9e827ea992102a5b43d0e34
jun-hy <dlvm789@gmail.com>
2025-05-12 16:25:02 +0900

    refactor: sqlite3 with async

--- db/database.ts
+++ db/database.ts
@@ -1,15 +1,12 @@
-import { Database, verbose } from "sqlite3";
-import path from "path";
+import sqlite3, { verbose } from 'sqlite3';
+import { open } from 'sqlite';
+import path from 'path';
 
 verbose();
 
-const dbPath = path.join(__dirname, '../data/database.sqlite3'); // DB 파일 위치
-const db = new Database(dbPath, (err) => {
-    if (err) {
-        console.error('Could not connect to database', err);
-    } else {
-        console.log('Connected to SQLite database');
-    }
-});
-
-export default db;
+export const initDB = () => {
+    return open({
+        filename: path.join(__dirname, '../data/database.sqlite3'),
+        driver: sqlite3.Database,
+    });
+};
